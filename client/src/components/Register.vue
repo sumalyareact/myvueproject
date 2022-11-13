dea4f23003408d2f3a4cd9ad772b06fffe6b52d7
shubham sumalya <shubhamsumalya92@gmail.com>
2022-11-13 16:14:54 +0100

    created Api

---
+++ client/src/components/Register.vue
@@ -0,0 +1,56 @@
+<template>
+ <div>
+
+  <h1>Register</h1>
+
+  <input 
+  type="email"
+  name="email"
+  v-model="email"
+  placeholder="email"/> 
+
+  <br>
+  <br>
+  <br>
+
+  <input 
+  type="password"
+  name="password"
+  v-model="password"
+  placeholder="password"/> 
+
+  <br>
+  <br> 
+
+  <button @click="register">Register</button>
+
+</div>
+</template>
+
+<script>
+import Authenticationservice from '@/services/Authenticationservice'
+export default {
+    data(){
+        return{
+            email:'',
+            password:''
+        }
+
+    },
+    methods:{
+        async register(){
+             await Authenticationservice.register({
+                email: this.email,
+                password: this.password
+            })
+            
+
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
